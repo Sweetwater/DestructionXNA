ab0b549cdd16efddaab45f683fd1424a719a49ec
Sweetwater <sweeeetwater@gmail.com>
2010-05-28 16:56:44 +0900

    3DEngine First Commit

--- DestructionXNA/Game1.cs
+++ DestructionXNA/Game1.cs
@@ -12,29 +12,29 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
-using FarseerGames.FarseerPhysics;
-using FarseerGames.FarseerPhysics.Dynamics;
-using FarseerGames.FarseerPhysics.Collisions;
-using FarseerPhysicsTutorial1;
-using FarseerGames.FarseerPhysics.Dynamics.Joints;
-using FarseerGames.FarseerPhysics.Dynamics.Springs;
 using DebugTool;
-using FarseerGames.FarseerPhysics.Factories;
+using JigLibX.Physics;
+using JigLibX.Collision;
+using DestructionXNA.PhysicObjects;
 
 namespace DestructionXNA
 {
-
+    public class Context
+    {
+        public Context(Camera camera, DebugDrawer debugDrawer)
+        {
+            this.Camera = camera;
+            this.DebugDrawer = debugDrawer;
+        }
+        public readonly Camera Camera;
+        public readonly DebugDrawer DebugDrawer;
+    }
+    
     /// <summary>
     /// This is the main type for your game
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
-        enum State {
-            Start,
-            Play,
-        }
-        State state;
-
         GraphicsDeviceManager graphics;
 
         SpriteBatch spriteBatch;
@@ -42,10 +42,9 @@ namespace DestructionXNA
             get { return spriteBatch; }
         }
 
-        PhysicsSimulator physicsSimulator;
-        public PhysicsSimulator PhysicsSimulator{
-            get { return physicsSimulator; }
-        }
+        Context context;
+
+        PhysicsSystem physicSystem = new PhysicsSystem();
 
         Matrix projection;
         public Matrix Projection {
@@ -63,12 +62,6 @@ namespace DestructionXNA
             get { return inputState; }
         }
 
-
-        Drawer drawer;
-        public Drawer Drawer {
-            get { return drawer; }
-        }
-
         public int ScreenWidth {
             get { return 640; }
         }
@@ -78,15 +71,6 @@ namespace DestructionXNA
         }
 
         Random random;
-
-        Player player;
-        Controller controller;
-
-        Body groundBody;
-        Geom groundGeom;
-
-        Body[] blockBodys;
-        Geom[] blockGeoms;
 
         public Game1()
         {
@@ -103,92 +87,36 @@ namespace DestructionXNA
 
             random = new Random();
             inputState = new InputState();
+
+            InitializePhysicsSystem();
+
+            CreateContext();
         }
 
-
-        private int rectNum = 5;
-        private int circleNum = 5;
-        private int triNum = 5;
-
-        private float fluctuation = 3;
-
-        private float rectWidth = 20;
-        private float rectHeight = 20;
-        private float rectMass = 1;
-
-        private float circleRadius = 10;
-        private int circleEdge = 10;
-
-        private float blockY = 50;
-
-        private void CreateBlock()
+        private void CreateContext()
         {
-            //blockBodys = new Body[rectNum + circleNum + triNum];
-            //blockGeoms = new Geom[rectNum + circleNum + triNum];
-            blockBodys = new Body[rectNum];
-            blockGeoms = new Geom[rectNum];
-
-            CreateRectBlock();
-            //CreateCircleBlock();
+            var camera = new Camera(this);
+            context = new Context(new Camera(this), new DebugDrawer(this, camera));
+            Components.Add(context.Camera);
+            //Components.Add(context.DebugDrawer);
         }
 
-        private void CreateRectBlock()
+        private void InitializePhysicsSystem()
         {
-            for (int i = 0; i < rectNum; i++)
-            {
-                float width = rectWidth * GetRandom(1, fluctuation);
-                float height = rectHeight * GetRandom(1, fluctuation);
-                float mass = rectMass * GetRandom(1, fluctuation);
-                Vector2 position = new Vector2(GetRandom(-ScreenWidth / 2, ScreenWidth / 2), blockY);
+            physicSystem.CollisionSystem = new CollisionSystemSAP();
 
-                blockBodys[i] = BodyFactory.Instance.CreateRectangleBody(
-                    physicsSimulator,
-                    width, height, mass);
-                blockGeoms[i] = GeomFactory.Instance.CreateRectangleGeom(
-                    physicsSimulator,
-                    blockBodys[i], width, height);
+            physicSystem.EnableFreezing = true;
+            physicSystem.SolverType = PhysicsSystem.Solver.Normal;
+            physicSystem.CollisionSystem.UseSweepTests = true;
 
-                blockGeoms[i].FrictionCoefficient = 0.8f;
-                blockBodys[i].Position = position;
-            }
-
+            physicSystem.NumCollisionIterations = 10;
+            physicSystem.NumContactIterations = 10;
+            physicSystem.NumPenetrationRelaxtionTimesteps = 15;
         }
-
-        private void CreateCircleBlock()
-        {
-            for (int i = 0; i < circleNum; i++)
-            {
-                float radius = circleRadius * GetRandom(1, fluctuation);
-                float mass = rectMass * GetRandom(1, fluctuation);
-                Vector2 position = new Vector2(GetRandom(-ScreenWidth / 2, ScreenWidth / 2), blockY);
-
-                blockBodys[i] = BodyFactory.Instance.CreateCircleBody(
-                    physicsSimulator,
-                    radius, mass);
-                blockGeoms[i] = GeomFactory.Instance.CreateCircleGeom(
-                    physicsSimulator,
-                    blockBodys[i], radius, circleEdge);
-
-                blockBodys[i].Position = position;
-            }
-        }
-
 
         private float GetRandom(float min, float max)
         {
             return min + (float)random.NextDouble() * (max - min);
-        }
-
-        private void CreateGround()
-        {
-            groundBody = BodyFactory.Instance.CreateRectangleBody(physicsSimulator, 600, 20, 1);
-            groundBody.IsStatic = true;
-
-            groundGeom = GeomFactory.Instance.CreateRectangleGeom(physicsSimulator, groundBody, 600, 20);
-            groundGeom.RestitutionCoefficient = 0.2f;
-            groundGeom.FrictionCoefficient = 0.8f;
-
-            groundBody.Position = new Vector2(0, 230);
         }
 
         /// <summary>
@@ -223,21 +151,8 @@ namespace DestructionXNA
             float viewHeight = GraphicsDevice.Viewport.Height;
             projection = Matrix.CreateOrthographicOffCenter(-viewWidth / 2, viewWidth / 2, viewHeight / 2, -viewHeight / 2, -10, 10);
 
-            drawer = new Drawer(GraphicsDevice);
-
-            physicsSimulator = new PhysicsSimulator(new Vector2(0, 100));
-            CreateGround();
-            CreateBlock();
-
-
-            player = new Player(
-                this,
-                Content.Load<Texture2D>("niconicoTVchan64"),
-                Content.Load<Model>("anttenaChip"));
-
-            controller = new Controller(this, player, blockBodys);
-
-            state = State.Start;
+            InitializeScene();
+            AddCrate();
         }
 
         /// <summary>
@@ -262,44 +177,24 @@ namespace DestructionXNA
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
-#if DEBUG && DEBUG_WINDOW            
-            if (this.IsActive &&
-                inputState.NewMouseState.LeftButton == ButtonState.Pressed) {
+            // TODO: Add your update logic here
+#if DEBUG && DEBUG_WINDOW
+            {
+                if (this.IsActive &&
+                    inputState.NewMouseState.LeftButton == ButtonState.Pressed)
+                {
 
-                float x = inputState.NewMouseState.X - 320;
-                float y = inputState.NewMouseState.Y - 240;
-                debugWindow.SetPositionText(x, y);
+                    float x = inputState.NewMouseState.X - 320;
+                    float y = inputState.NewMouseState.Y - 240;
+                    debugWindow.SetPositionText(x, y);
+                }
             }
 #endif
+            HandleInput();
 
-            // TODO: Add your update logic here
-            switch (state)
-            {
-                case State.Start:
-                    if (inputState.IsTrigger(Keys.Enter)) {
-                        state = State.Play;
-                    }
-                    break;
-                case State.Play:
-                    UpdatePlay(gameTime);
-                    break;
-                default:
-                    break;
-            }
+            physicSystem.Integrate((float)gameTime.ElapsedGameTime.TotalSeconds);
 
             base.Update(gameTime);
-        }
-
-        private void UpdatePlay(GameTime gameTime)
-        {
-            controller.Update();
-            player.Update(gameTime);
-            physicsSimulator.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
-        }
-
-        void Draw(Geom geom)
-        {
-            drawer.DrawClosedPolyline(geom.WorldVertices.GetVerticesArray());
         }
 
         /// <summary>
@@ -311,18 +206,158 @@ namespace DestructionXNA
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            drawer.Begin(projection);
-            Draw(groundGeom);
-
-            foreach (Geom blockGeom in blockGeoms)
-            {
-                Draw(blockGeom);
-            }
-            drawer.End();
-
-            player.Draw();
 
             base.Draw(gameTime);
+        }
+
+    
+
+        readonly List<PhysicObject> items = new List<PhysicObject>();
+        //List<Constraint> constraints = new List<Constraint>();
+        //List<HingeJoint> joints = new List<HingeJoint>();
+        readonly List<Controller> controllers = new List<Controller>();
+        //PhysicObject currentItem;
+
+
+        void Add(PhysicObject item)
+        {
+            Components.Add(item);
+            items.Add(item);
+            var controller = new SampleController(item.PhysicsBody, new Vector3(0, 12, 0), Vector3.Zero);
+            controller.EnableController();
+            controllers.Add(controller);
+        }
+
+        //void Add(PhysicObject item, Vector3 hingePosition)
+        //{
+        //    Components.Add(item);
+        //    items.Add(item);
+
+        //    var previousItem = currentItem;
+        //    currentItem = item;
+        //    if (previousItem != null)
+        //    {
+        //        // ジョイントを作成する
+        //        var joint = new HingeJoint();
+        //        joint.Initialise(
+        //            previousItem.PhysicsBody, currentItem.PhysicsBody,
+        //            new Vector3(0, 0, 1),
+        //            hingePosition, 1.0f, 180, -180, 0.1f, 0.5f);
+        //        joint.EnableHinge();
+        //        joints.Add(joint);
+        //    }
+        //}
+
+		void InitializeScene() {
+			// カメラを設定する
+			context.Camera.Position = new Vector3(0, 10, 10);
+			context.Camera.Target = new Vector3(0, 0, 0);
+
+			// 古いボディとそのコンポーネントを削除する
+			foreach (var item in items) {
+				Components.Remove(item);
+				item.PhysicsBody.DisableBody();
+				item.Dispose();
+			}
+			items.Clear();
+
+
+            //foreach (var joint in joints)
+            //{
+            //    joint.DisableController();
+            //}
+            //joints.Clear();
+
+            //currentItem = null;
+
+            foreach (var controller in controllers)
+            {
+                physicSystem.RemoveController(controller);
+            }
+            controllers.Clear();
+
+
+            // 重力
+			physicSystem.Gravity = new Vector3(0, -9.8f, 0);
+
+			// 固定の床を作る
+			var floor = new BoxObject(this, context, Content.Load<Model>("Crate"),
+				new Vector3(10, 1, 10), Matrix.Identity, Vector3.Zero);
+			floor.PhysicsBody.Immovable = true;
+            Add(floor);
+            //Add(floor, Vector3.Zero);
+
+            //AddCrate(new Vector3(3, 0, 0), new Vector3(0.0f, 0, 0));
+            //AddCrate(new Vector3(6, 0, 0), new Vector3(1.5f, 0, 0));
+            //AddCrate(new Vector3(9, 0, 0), new Vector3(1.5f, 0, 0));
+            //AddCrate(new Vector3(12, 0, 0), new Vector3(1.5f, 0, 0));
+            //AddCrate(new Vector3(15, 0, 0), new Vector3(1.5f, 0, 0));
+        }
+		/// <summary>
+		/// 木箱を追加する。
+		/// </summary>
+        void AddCrate()
+        {
+            var crate = new BoxObject(this, context, Content.Load<Model>("Crate"),
+                new Vector3(1, 1, 1),
+                Matrix.CreateRotationX((float)random.NextDouble()) *
+                Matrix.CreateRotationY((float)random.NextDouble()), new Vector3(0, 5, 0));
+            Add(crate);
+        }
+        //void AddCrate(Vector3 position, Vector3 hingePosition)
+        //{
+        //    var crate = new BoxObject(this, context, Content.Load<Model>("Crate"),
+        //        new Vector3(2, 0.25f, 2), Matrix.Identity, position);
+        //    Add(crate, hingePosition);
+        //}
+
+	#region 入力関連
+	    KeyboardState previous;
+	    KeyboardState current;
+	    bool IsKeyPress(Keys key) {
+		    return current.IsKeyDown(key) && previous.IsKeyUp(key);
+	    }
+	    public void HandleInput() {
+		    previous = current;
+		    current = Keyboard.GetState();
+            if (IsKeyPress(Keys.Space)) AddCrate();
+            if (IsKeyPress(Keys.X))
+            {
+                foreach (var controller in controllers)
+                    controller.EnableController();
+            }
+            if (IsKeyPress(Keys.Z))
+            {
+                foreach (var controller in controllers)
+                    controller.DisableController();
+            }
+            if (IsKeyPress(Keys.Delete)) InitializeScene();
+	    }
+	#endregion
+
+    }
+
+    class SampleController : Controller
+    {
+        Body body;
+        Vector3 force;
+        Vector3 torque;
+
+        public SampleController(Body body, Vector3 force, Vector3 torque)
+        {
+            this.body = body;
+            this.force = force;
+            this.torque = torque;
+        }
+        public override void UpdateController(float dt)
+        {
+            if (body == null) return;
+            if (force != Vector3.Zero || torque != Vector3.Zero)
+            {
+                body.AddWorldForce(force);
+                body.AddWorldTorque(torque);
+                if (!body.IsActive) body.SetActive();
+            }
         }
     }
 }
---
+++ DestructionXNA/InputState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace DestructionXNA
+{
+    public class InputState
+    {
+        public KeyboardState NewKeyboardState { get; private set;}
+        public KeyboardState OldKeyboardState { get; private set; }
+
+        public MouseState NewMouseState { get; private set; }
+        public MouseState OldMouseState { get; private set; }
+
+        public InputState() {
+        }
+
+        public void Update() {
+            OldKeyboardState = NewKeyboardState;
+            OldMouseState = NewMouseState;
+
+            NewKeyboardState = Keyboard.GetState();
+            NewMouseState = Mouse.GetState();
+        }
+
+        public bool IsDown(Keys key) {
+            return NewKeyboardState.IsKeyDown(key);
+        }
+        public bool IsUp(Keys key)
+        {
+            return NewKeyboardState.IsKeyUp(key);
+        }
+        public bool IsTrigger(Keys key)
+        {
+            return IsDown(key) && OldKeyboardState.IsKeyUp(key);
+        }
+        public bool IsRelase(Keys key)
+        {
+            return IsUp(key) && OldKeyboardState.IsKeyDown(key);
+        }
+    }
+}
